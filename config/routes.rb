1d611f2278ea5294679527a9380d8c119b637e54
takaya-47 <local.s.s.47@gmail.com>
2021-01-29 15:54:46 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'messages/index'
   root to: "messages#index"
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:edit, :update] # devise_for :usersでは定義されていない部分のルーティングを補う
+  resources :rooms, only: [:new, :create]
 end
