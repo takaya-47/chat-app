b4229dabf7e0522ae441e7e88ae434cf4657d08e
takaya-47 <local.s.s.47@gmail.com>
2021-01-29 18:27:38 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'messages/index'
-  root to: "messages#index"
+  root to: "rooms#index"
   resources :users, only: [:edit, :update] # devise_for :usersでは定義されていない部分のルーティングを補う
   resources :rooms, only: [:new, :create]
 end
