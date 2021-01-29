59fd45c560b52b2d9a174e1f491b9b19e9179a20
takaya-47 <local.s.s.47@gmail.com>
2021-01-29 17:49:15 +0900

    プルダウンの実装

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,4 @@
 class Room < ApplicationRecord
-  has_many :users, through: room_users
-  has_many :room_users
+  has_many :room_users # こちらを先に書かないといけない
+  has_many :users, through: :room_users
 end
